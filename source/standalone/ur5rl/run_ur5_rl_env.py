96de09070b318636158093b029a657a24ba61890
Luca <luca.j@online.de>
2025-01-25 16:58:19 +0100

    cube position logging

--- source/standalone/ur5rl/run_ur5_rl_env.py
+++ source/standalone/ur5rl/run_ur5_rl_env.py
@@ -119,6 +119,19 @@ def store_joint_positions(
     )
 
 
+def store_cube_positions(
+    logger: InfluxDataLogger,
+    cube_position_tracked: list,
+    cube_position_gt: list,
+    bucket: str,
+):
+    logger.store_cube_positions(
+        cube_position_tracked=cube_position_tracked,
+        cube_position_gt=cube_position_gt,
+        bucket=bucket,
+    )
+
+
 # ---------------------------------------
 
 
@@ -135,14 +148,14 @@ def sync_sim_joints_with_real_robot(env: HawUr5Env, ur5_controller: Ur5JointCont
 # SETUP VARS ----------------
 args_cli.pub2ros = False
 args_cli.log_data = False
-args_cli.num_envs = 1
+args_cli.num_envs = 5
 args_cli.pp_setup = True
 # ---------------------------
 
 
 def main():
     """Main function."""
-    elbow_lift = 0.0
+    elbow_lift = -0.2
     wrist1_lift = 0.0
 
     ### Get run configurations
@@ -202,7 +215,7 @@ def main():
         -1.0,
     ]
     pd = [x - y for x, y in zip(pos2, pos1)]
-    pd_step = pd  # [x / 10 for x in pd]
+    pd_step = [x / 4 for x in pd]
 
     while simulation_app.is_running():
         with torch.inference_mode():
@@ -215,59 +228,30 @@ def main():
                 if PUBLISH_2_ROS:
                     sync_sim_joints_with_real_robot(env, ur5_controller)
 
-            #! DEBUG STATEMENT
-            # Set to debug pos
-            # - -1.1613042990313929
-            # - 1.98795747756958
-            # - -3.9438589254962366
-            # - -1.5171244780169886
-            # - -0.0027774016009729507
-            # - -0.005194489155904591
-
+            #! DEBUG STATEMENT ----------------
             if count == 0:
                 # Set gripper to cube hight parallel to the table
-                env.set_joint_angles_absolute(
-                    joint_angles=[  # type: ignore
-                        -0.0,
-                        -1.1612923781024378,
-                        1.98793363571167,
-                        -3.9438589254962366,
-                        -1.5171125570880335,
-                        -0.0027774016009729507,
-                        -1.0,
-                    ]
-                )
-                # env.set_joint_angles_absolute(
-                #     joint_angles=[  # type: ignore
-                #         -0.0,
-                #         -1.1,
-                #         1.0,
-                #         -2.25,
-                #         -1.5171125570880335,
-                #         -0.0027774016009729507,
-                #         -1.0,
-                #     ]
-                # )
-            if count > 200 and count < 350:
+                env.set_joint_angles_absolute(joint_angles=pos1)  # type: ignore
+            if count > 500 and count < 1100:
                 actions = pd_step
             else:
                 actions = [0.0] * 7
-            #! DEBUG STATEMENT
+            #! ^^^DEBUG STATEMENT^^^ ----------
             # Get test action for the gripper
             gripper_action = -1 + count / 100 % 2
             # create a tensor for joint position targets with 7 values (6 for joints, 1 for gripper)
             actions = torch.tensor(
                 [
-                    actions
-                    # [
-                    #     0.0,
-                    #     0.0,
-                    #     elbow_lift,
-                    #     wrist1_lift,  # wrist1,
-                    #     0.0,
-                    #     0.0,
-                    #     -1,  # gripper_action,
-                    # ]
+                    # actions
+                    [
+                        0.0,
+                        0.0,
+                        elbow_lift,
+                        wrist1_lift,  # wrist1,
+                        0.0,
+                        0.0,
+                        -1,  # gripper_action,
+                    ]
                 ]
                 * env_cfg.scene.num_envs
             )
@@ -297,6 +281,17 @@ def main():
 
             # Step the environment
             obs, rew, terminated, truncated, info = env.step(actions)
+
+            # Log the cube positions
+            if LOG_DATA:
+                if count > 500 and count < 1100:
+                    cube_position_tracked = obs["cube_pos"][0].cpu().numpy().tolist()
+                    store_cube_positions(
+                        logger=logger,
+                        cube_position_tracked=cube_position_tracked,
+                        cube_position_gt=[1.0, 0.1, 0.25],
+                        bucket="cube_pos",
+                    )
 
             obs_vals: torch.Tensor = obs["policy"]  # type: ignore
 
